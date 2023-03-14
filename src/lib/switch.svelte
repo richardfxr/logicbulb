bb7bc3758aec98fd84b11c03b9d1d1caea2c9d68
Richard Fu <richardfxr.personal@gmail.com>
2023-03-13 23:38:01 -0400

    🟠SVLT: create switch component

---
+++ src/lib/switch.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+    /* === PROPS ============================== */
+    export let a11yLabel: string;
+    export let output = false;
+</script>
+
+
+
+<div class="cell">
+    <label
+        class="switch"
+        class:active={output}>
+        <input
+            class="visuallyHidden"
+            type="checkbox"
+            bind:checked={output}>
+        <span class="visuallyHidden">{a11yLabel}</span>
+        <span class="top">{output ? "1" : "0"}</span>
+    </label>
+</div>
+
+
+
+
+<style lang="scss">
+    .cell {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    label {
+        cursor: pointer;
+        display: block;
+        position: relative;
+
+        font-family: sans-serif;
+        font-size: 1.8rem;
+
+        margin-top: calc(-1 * var(--input-transY-hover));
+
+        // prevent text selection
+        -webkit-user-select: none; // Safari
+        user-select: none;
+
+        &::before {
+            content: "";
+            position: absolute;
+            inset: 0;
+            z-index: -2;
+
+            background-color: #ED9718;
+            border-radius: var(--border-radius);
+            border: var(--border);
+        }
+
+        .top {
+            display: block;
+            position: relative;
+
+            padding: 21px 13px 9px 13px;
+            border-radius: var(--border-radius) var(--border-radius) 0 0;
+            border-top: var(--border);
+            border-right: var(--border);
+            border-left: var(--border);
+
+            transform: translateY(var(--input-transY));
+
+            transition: transform var(--trans-input);
+
+            &::before {
+                content: "";
+                position: absolute;
+                inset: 0;
+                z-index: -1;
+
+                background: linear-gradient(0deg, #FFE0B3 0%, #FFCF8A 80%);
+                border-radius: calc(var(--border-radius) - var(--border-width));
+            }
+        }
+
+        &:hover, &:focus {
+            .top {
+                transform: translateY(var(--input-transY-hover));
+            }
+        }
+
+        &:active, &.active {
+            .top {
+                transform: translateY(var(--input-transY-active));
+            }
+        }
+
+        &.active {
+            &:hover, &:focus {
+                .top {
+                    transform: translateY(var(--input-transY-active-hover));
+                }
+            }
+
+            &:active {
+                .top {
+                    transform: translateY(var(--input-transY-active));
+                }
+            }
+        }
+    }
+
+    input {
+        visibility: hidden;
+    }
+</style>
---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,198 @@
+<script lang="ts">
+    /* === IMPORTS ============================ */
+    import Switch from "$lib/switch.svelte";
+    
+    /* === REFS =============================== */
+    let sticky: HTMLElement;
+    let main: HTMLElement;
+
+    /* === VARIABLES ========================== */
+    let scrollPos = 0; // stores the current scrollLeft value to prevented repeated on:scroll calls
+    let switch1 = false;
+    let switch2 = false;
+    let switch3 = false;
+    let switch4 = false;
+    let switch5 = false;
+    let switch6 = false;
+    let switch7 = false;
+    let switch8 = false;
+</script>
+
+
+
+<div bind:this={sticky} class="container" id="sticky" on:scroll={e => {
+    // return if already at scrollPos to prevent on:scroll loop
+    if (sticky.scrollLeft === scrollPos) return;
+
+    main.scrollLeft = sticky.scrollLeft;
+    scrollPos = sticky.scrollLeft
+    console.log("scrolled main");
+}}>
+    <ol class="inputs">
+        <li class="input">
+            <Switch
+                a11yLabel="switch 1"
+                bind:output={switch1} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 2"
+                bind:output={switch2} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 3"
+                bind:output={switch3} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 4"
+                bind:output={switch4} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 5"
+                bind:output={switch5} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 6"
+                bind:output={switch6} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 7"
+                bind:output={switch7} />
+        </li>
+        <li class="input">
+            <Switch
+                a11yLabel="switch 8"
+                bind:output={switch8} />
+        </li>
+    </ol>
+</div>
+
+<div bind:this={main} class="container" on:scroll={e => {
+    // return if already at scrollPos to prevent on:scroll loop
+    if (main.scrollLeft === scrollPos) return;
+
+    sticky.scrollLeft = main.scrollLeft;
+    scrollPos = main.scrollLeft;
+    console.log("scrolled sticky");
+}}>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+    <ol class="inputs">
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+        <li class="input"></li>
+    </ol>
+</div>
+
+
+
+<style lang="scss">
+    :global {
+        body {
+            background-color: var(--clr-100);
+        }
+    }
+    .container {
+        display: flex;
+        flex-flow: column nowrap;
+
+        overflow: auto;
+    }
+
+    ol.inputs {
+        list-style-type: none;
+        display: grid;
+        grid-template-columns: repeat(8, var(--cell-width));
+
+        padding: 0 15px;
+        margin: 0 auto 0 auto;
+    }
+
+    #sticky {
+        position: sticky;
+        top: -40px;
+    }
+
+    li {
+        display: block;
+        height: var(--cell-height);
+    }
+</style>
