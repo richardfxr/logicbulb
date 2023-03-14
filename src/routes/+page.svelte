43909b70d8a8bcf30bd9ebdee7a782fca41707ca
Richard Fu <richardfxr.personal@gmail.com>
2023-03-13 23:44:20 -0400

    🟠SVLT: add basic index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <h1>logicbulb</h1>
+    <p>A puzzle game with logic gates. Currently in development.</p>
+</main>
+
+
+
+
+<style lang="scss">
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        font-family: sans-serif;
+
+        padding: 20px;
+    }
+
+    h1 {
+
+    }
+</style>
